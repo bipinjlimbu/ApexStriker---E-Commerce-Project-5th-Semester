cb635531661d611f98514509c99958bd6ebd03b3
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-22 18:12:34 +0545

    Home Page Updated

--- as_app/urls.py
+++ as_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views.auth_view import register_view
-from .views.main_view import home_page
+from .views.main_view import home_view
 
 urlpatterns = [
-    path('', home_page, name='home_page'),
+    path('', home_view, name='home_page'),
     path('register/', register_view, name='register_page'),
 ]
