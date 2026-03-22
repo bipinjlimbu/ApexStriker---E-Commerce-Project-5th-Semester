54a62aa4ce88f03e61d8658046fd46aa7aada3e5
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-22 17:57:54 +0545

    Register Page Setup Done

--- as_app/urls.py
+++ as_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views.auth_view import register_view
 from .views.main_view import home_page
 
 urlpatterns = [
     path('', home_page, name='home_page'),
+    path('register/', register_view, name='register_page'),
 ]
