224e8a41d8d0c6e2972a425ea6359b4adafbe6ac
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-23 20:26:40 +0545

    Edit Profile Page Setup Done

--- as_app/views/profile_view.py
+++ as_app/views/profile_view.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from ..models import User
 
+@login_required
 def profile_view(request, user_id):
     profile = User.objects.get(id=user_id)
     return render(request, 'main/profile_page.html', {'profile': profile})
+
+@login_required
+def edit_profile_view(request, user_id):
+    return render(request, 'main/edit_profile_page.html')
