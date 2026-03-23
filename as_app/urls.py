963afa3fdaddc830febee596fe75d87227c37955
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-23 20:20:12 +0545

    Profile Page Trough User ID Updated

--- as_app/urls.py
+++ as_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('register/', register_view, name='register_page'),
     path('login/', login_view, name='login_page'),
     path('logout/', logout_view, name='logout_page'),
-    path('profile/', profile_view, name='profile_page'),
+    path('profile/<int:user_id>/', profile_view, name='profile_page'),
 ]
--- as_app/views/main_view.py
+++ as_app/views/main_view.py
@@ -4,6 +4,6 @@ from ..models import User
 def home_view(request):
     return render(request, 'main/home_page.html')
 
-def profile_view(request):
-    profile = User.objects.get(id=request.user.id)
+def profile_view(request, user_id):
+    profile = User.objects.get(id=user_id)
     return render(request, 'main/profile_page.html', {'profile': profile})
