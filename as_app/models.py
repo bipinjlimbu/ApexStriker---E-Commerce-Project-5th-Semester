6f9abb4bd3163d183b44dfa50bc2800c5e0b4c94
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-23 20:15:26 +0545

    Function Added in User Model

--- as_app/models.py
+++ as_app/models.py
@@ -12,6 +12,11 @@ class User(AbstractUser):
     profile_picture = models.ImageField(upload_to='images/user/', blank=True, null=True)
     is_verified = models.BooleanField(default=False)
     
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = self.Role.ADMIN
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return f"{self.username},{self.role}"
     
