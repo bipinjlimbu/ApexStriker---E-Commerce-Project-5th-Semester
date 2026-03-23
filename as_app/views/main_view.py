2a1913b593115f0bc1266238b3fb8a3a973ce1a3
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-23 19:55:33 +0545

    User Data Fetched in Profile View

--- as_app/views/main_view.py
+++ as_app/views/main_view.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from ..models import User
 
 def home_view(request):
     return render(request, 'main/home_page.html')
 
 def profile_view(request):
-    return render(request, 'main/profile_page.html')
+    profile = User.objects.get(id=request.user.id)
+    return render(request, 'main/profile_page.html', {'profile': profile})
