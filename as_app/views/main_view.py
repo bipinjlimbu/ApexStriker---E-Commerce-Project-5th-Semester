0ce46697f25fc1d0282bcd0f88f7c4c7ce95a19f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-24 19:21:37 +0545

    Verify Email View Added

--- as_app/views/main_view.py
+++ as_app/views/main_view.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
 from ..models import User
 
 def home_view(request):
     return render(request, 'main/home_page.html')
+
+def verify_email_view(request, token):
+    try:
+        user = User.objects.get(auth_token=token)
+        if user.is_verified:
+            messages.info(request, "Your email is already verified.")
+        else:
+            user.is_verified = True
+            user.auth_token = None
+            user.save()
+            messages.success(request, "Email verification successful! You can now log in.")
+    except User.DoesNotExist:
+        messages.error(request, "Invalid verification token.")
+    
+    return render(request, 'main/verification_result.html')
