c09512ae6508104dbc788b8dadd6546789eb4040
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-24 19:22:36 +0545

    Verify Email Url Added

--- as_app/urls.py
+++ as_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
-from .views.main_view import home_view
+from .views.main_view import home_view, verify_email_view
 from .views.profile_view import profile_view, edit_profile_view
 
 urlpatterns = [
     path('', home_view, name='home_page'),
     path('register/', register_view, name='register_page'),
+    path('verify/<str:token>/', verify_email_view, name='verify_email'),
     path('login/', login_view, name='login_page'),
     path('logout/', logout_view, name='logout_page'),
     path('profile/<int:user_id>/', profile_view, name='profile_page'),
