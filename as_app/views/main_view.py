9a41d80c5ef98669873b1287bad3b8525e601806
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-25 18:15:06 +0545

    Verify Email View Updated

--- as_app/views/main_view.py
+++ as_app/views/main_view.py
@@ -6,16 +6,29 @@ def home_view(request):
     return render(request, 'main/home_page.html')
 
 def verify_email_view(request, token):
+    user = None
+    status = 'error'
+    
     try:
         user = User.objects.get(auth_token=token)
-        if user.is_verified:
-            messages.info(request, "Your email is already verified.")
-        else:
+        
+        if not user.is_verified:
             user.is_verified = True
-            user.auth_token = None
+            user.auth_token = None  # The "Burn"
             user.save()
-            messages.success(request, "Email verification successful! You can now log in.")
+            messages.success(request, "Email verification successful!")
+        
+        status = 'success'
+        
     except User.DoesNotExist:
-        messages.error(request, "Invalid verification token.")
+        if request.user.is_authenticated and request.user.is_verified:
+            user = request.user
+            status = 'success'
+        else:
+            messages.error(request, "Invalid or expired verification token.")
+            status = 'error'
     
-    return render(request, 'main/verification_result_page.html', {'status': 'success' if user.is_verified else 'error', 'user': user})
+    return render(request, 'main/verification_result_page.html', {
+        'status': status, 
+        'user': user
+    })
