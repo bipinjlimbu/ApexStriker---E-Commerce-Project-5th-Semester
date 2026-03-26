650e14fdbfc2a9adbbdf990591b5b7e4f08144fb
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-26 19:15:59 +0545

    Approve and Reject Vendor Urls Added

--- as_app/urls.py
+++ as_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.auth_view import register_view, login_view, logout_view
 from .views.main_view import home_view, verify_email_view
 from .views.profile_view import profile_view, edit_profile_view, resend_verification_email, delete_profile_view
-from .views.dashboard import admin_dashboard_view, vendor_dashboard_view, customer_dashboard_view
+from .views.dashboard import admin_dashboard_view, approve_vendor_view, reject_vendor_view, vendor_dashboard_view, customer_dashboard_view
 
 urlpatterns = [
     path('', home_view, name='home_page'),
@@ -15,6 +15,8 @@ urlpatterns = [
     path('profile/resend_verification/<int:user_id>/', resend_verification_email, name='resend_verification_email'),
     path('profile/delete/<int:user_id>/', delete_profile_view, name='delete_profile_page'),
     path('dashboard/admin/', admin_dashboard_view, name='admin_dashboard'),
+    path('dashboard/approve_vendor/<int:vendor_id>/', approve_vendor_view, name='approve_vendor'),
+    path('dashboard/reject_vendor/<int:vendor_id>/', reject_vendor_view, name='reject_vendor'),
     path('dashboard/vendor/', vendor_dashboard_view, name='vendor_dashboard'),
     path('dashboard/customer/', customer_dashboard_view, name='customer_dashboard'),
 ]
