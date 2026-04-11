e5d766156d62bc150b3d3e6d9cb1f4cf378254a5
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-11 20:28:21 +0545

    Products Model Updated

--- as_app/models.py
+++ as_app/models.py
@@ -86,6 +86,7 @@ class Product(models.Model):
     vendor = models.ForeignKey('Vendor', on_delete=models.CASCADE, related_name='products')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL, null=True)
+    custom_brand = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
