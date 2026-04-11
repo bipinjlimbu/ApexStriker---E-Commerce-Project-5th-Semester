1ab97c56b540342dba314e6f18d56f23bc761169
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-11 21:04:26 +0545

    Product Image Model Added

--- as_app/models.py
+++ as_app/models.py
@@ -91,13 +91,16 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products/')
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/products/')
+    
 class Wishlist(models.Model):
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
