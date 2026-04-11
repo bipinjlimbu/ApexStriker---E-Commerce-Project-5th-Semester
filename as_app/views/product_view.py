650ffbc6b1a26509aa4bd81ff0290bdc203bae33
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-11 21:03:11 +0545

    Brand Fetched in Add Products Page View

--- as_app/views/product_view.py
+++ as_app/views/product_view.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from ..models import Product
+from ..models import Product, Brand, Vendor
 
 @login_required
 def add_product_view(request):
-    return render(request, 'main/add_product_page.html')
+    if request.user.role != 'vendor' and not Vendor.objects.filter(user=request.user, status=Vendor.Status.APPROVED).exists():
+        messages.error(request, "You are not authorized to add products.")
+        return redirect('/')
+    
+    brand = Brand.objects.all()
+    
+    return render(request, 'main/add_product_page.html', {'brands': brand})
