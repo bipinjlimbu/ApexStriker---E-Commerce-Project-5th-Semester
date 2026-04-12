28d2ac6395dc97b5840cac11c81a1c324b80b7e2
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-12 11:17:48 +0545

    Email Sender Added in Add Brand View

--- as_app/views/brand_view.py
+++ as_app/views/brand_view.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 from ..models import Brand
+import threading
 
+def send_email_async(subject, message, recipient):
+        try:
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [recipient], fail_silently=False)
+        except Exception as e:
+            print(f"Error sending email: {e}")
+            
 @login_required
 def add_brand_view(request):
     if not request.user.is_superuser:
@@ -28,6 +37,12 @@ def add_brand_view(request):
             
         if not errors:
             brand = Brand.objects.create(name=name, logo=logo, description=description)
+            
+            subject = "New Brand Added - ApexStriker"
+            message = f"Hi {request.user.username},\n\nThe brand '{brand.name}' has been successfully added to ApexStriker.\n\nThank you for contributing to our platform!"
+            email_thread = threading.Thread(target=send_email_async, args=(subject, message, request.user.email))
+            email_thread.start()
+
             messages.success(request, f"Brand '{brand.name}' has been added successfully.")
             return redirect('/brands/')
         
