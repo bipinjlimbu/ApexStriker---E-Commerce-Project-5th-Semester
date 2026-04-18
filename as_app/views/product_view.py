92906fce5e42ecb9257be2483492b776bb79a913
bipinjlimbu <bipinlimbu712@gmail.com>
2026-04-18 17:13:03 +0545

    In Wishlist Check Added in Single Product View

--- as_app/views/product_view.py
+++ as_app/views/product_view.py
@@ -243,6 +243,11 @@ def delete_product_view(request, product_id):
 def single_product_view(request, product_id):
     product = Product.objects.get(id=product_id)
     
+    if Wishlist.objects.filter(customer=request.user.customer_profile, product=product).exists():
+        product.in_wishlist = True
+    else:
+        product.in_wishlist = False
+        
     if request.method == 'POST':
         if not request.user.is_authenticated or request.user.role != 'customer':
             messages.error(request, "You must be logged in as a customer to add products to the cart.")
