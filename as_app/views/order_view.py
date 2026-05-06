7a9ce3eeb784a59f1cea3871b39c8b369eced402
bipinjlimbu <bipinlimbu712@gmail.com>
2026-05-06 17:29:13 +0545

    Dibursement Logic Added in Confirm Delivery View

--- as_app/views/order_view.py
+++ as_app/views/order_view.py
@@ -6,6 +6,7 @@ from django.conf import settings
 from ..models import Cart, Order, OrderItem, Disbursement
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.db import models
 import threading
 import json
 
@@ -136,10 +137,20 @@ def confirm_delivery_view(request, order_id):
         messages.error(request, f"Cannot confirm delivery for order #{order.id}. It has not been marked as shipped yet.")
         return redirect('/dashboard/customer/?section=my-orders')
     
+    sum_money = OrderItem.objects.filter(order=order, vendor=request.user.vendor_profile).aggregate(total=models.Sum('total_price'))['total'] or 0
+    
     order.status = Order.Status.COMPLETED
     order.save()
     
-    Disbursement
+    dibursement = Disbursement.objects.create(
+        order=order,
+        vendor=order.items.first().vendor,
+        admin_commission=sum_money * 0.10,
+        payout_amount=sum_money * 0.90,
+        is_transferred=False,
+        bank_ref_no=order.items.first().vendor.bank_account_number
+    )
+    dibursement.save()
     
     subject = "Order Delivery Confirmed - ApexStriker"
     message = f"Hi {order.customer.user.username},\n\nThank you for confirming the delivery of your order #{order.id}. We hope you enjoy your purchase!\n\nBest regards,\nApexStriker Team"
