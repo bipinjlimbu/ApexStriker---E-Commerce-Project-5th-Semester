3165806231517458f5491c450bc4ecb019bd5dbf
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-09 19:30:35 +0545

    Model Updated

--- as_app/models.py
+++ as_app/models.py
@@ -136,10 +136,15 @@ class Order(models.Model):
         SHIPPED = 'shipped', 'Shipped'
         COMPLETED = 'completed', 'Delivered & Received'
         CANCELLED = 'cancelled', 'Cancelled'
+        
+    class PaymentMethod(models.TextChoices):
+        ESEWA = 'esewa', 'eSewa'
+        COD = 'cod', 'Cash on Delivery'
 
     customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
     total_amount = models.DecimalField(max_digits=12, decimal_places=2)
     status = models.CharField(max_length=20, choices=Status.choices, default=Status.PAID)
+    payment_method = models.CharField(max_length=20, choices=PaymentMethod.choices, default=PaymentMethod.ESEWA)
     transaction_id = models.CharField(max_length=100, help_text="eSewa/Khalti Ref ID")
     shipping_address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
