c009c9c72bd889b9f3986308822d778e3c0417ce
bipinjlimbu <bipinlimbu712@gmail.com>
2026-08-10 18:28:06 +0545

    Wishlist Page Logic Setup Done

---
+++ as_app/views/wishlist_view.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from ..models import Wishlist, Product
+
+def wishlist_view(request):
+    return render(request, 'main/wishlist_page.html')
